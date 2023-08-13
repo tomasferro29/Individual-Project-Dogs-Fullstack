860c4e31a87c5c1af8808134804c301bf246e34d
Tomas Ferro <tomasferro29@gmail.com>
2023-08-13 14:15:14 -0300

    style deleted from app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
-import './App.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import { createGlobalStyle } from "styled-components"
 import Landing from './components/pages/Landing.jsx'
 import Home from './components/pages/Home.jsx';
 import Create from './components/pages/Create.jsx'
 import Detail from './components/pages/Detail.jsx'
-import Header from './components/pages/Header';
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
